1c4570aa16ce49a6a56f6bb57ae70c5680d780ef
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2022-01-26 15:07:34 +0800

    Added level and edited enemy functions

--- enemy.h
+++ enemy.h
@@ -1,13 +1,22 @@
 #pragma once
 
 #include "AEEngine.h"
+#include "globals.h"
+
+struct Enemy {
+	AEVec2 enemyPos;
+
+};
+
+extern AEGfxVertexList* pEnemyMesh;
+extern AEGfxTexture* pEnemyTexture;
 
 // Add or edit functions as required
 
-void enemy_load();
+void enemy_load(const char* texture_filename, float startPosX, float startPosY, float width, float height);
 
 void enemy_update();
 
-void enemy_draw();
+void enemy_draw(AEVec2& position, AEGfxTexture*& texture, AEGfxVertexList*& mesh);
 
-void enemy_unload();
+void enemy_unload(AEGfxTexture*& texture);
