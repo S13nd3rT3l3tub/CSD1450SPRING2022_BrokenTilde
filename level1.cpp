e3b2e3e03fbf11db5f065c3dc1550193e6a9d554
LEE HSIEN WEI JOACHIM LEE <joachimlee1999@gmail.com>
2022-01-30 22:37:57 +0800

    Added Level1 Functions

--- level1.cpp
+++ level1.cpp
@@ -1,1 +1,25 @@
 #include "Level1.h"
+
+void Level1_Load() {
+
+}
+
+void Level1_Initialize() {
+
+}
+
+void Level1_Update() {
+
+}
+
+void Level1_Draw() {
+
+}
+
+void Level1_Free(){
+
+}
+
+void Level1_Unload() {
+
+}
--- level1.h
+++ level1.h
@@ -2,4 +2,14 @@
 
 #include "GameStateManager.h"
 
-void level1_load();
+void Level1_Load();
+
+void Level1_Initialize();
+
+void Level1_Update();
+
+void Level1_Draw();
+
+void Level1_Free();
+
+void Level1_Unload();
