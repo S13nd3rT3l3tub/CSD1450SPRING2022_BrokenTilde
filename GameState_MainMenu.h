6a6ffd9c710edce643382b866841ddcc5ff4d229
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2022-03-05 16:58:28 +0800

    Main Menu added

---
+++ GameState_MainMenu.h
@@ -0,0 +1,31 @@
+/******************************************************************************/
+/*!
+\file		GameState_Level1.h
+\author 	DigiPen
+\par    	email: digipen\@digipen.edu
+\date   	January 01, 20xx
+\brief		ToDo: give a brief explanation here
+
+Copyright (C) 20xx DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+ */
+ /******************************************************************************/
+
+#ifndef GAME_STATE_MAINMENU_H_
+#define GAME_STATE_MAINMENU_H_
+
+// ---------------------------------------------------------------------------
+
+void GameStateMainMenuLoad(void);
+void GameStateMainMenuInit(void);
+void GameStateMainMenuUpdate(void);
+void GameStateMainMenuDraw(void);
+void GameStateMainMenuFree(void);
+void GameStateMainMenuUnload(void);
+
+// ---------------------------------------------------------------------------
+
+#endif 
+
+
